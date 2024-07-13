5445f7e3ab4b76b22573b5acdce86869dbb6a9a9
BrianRainbowAppears <wowangle1234@gmail.com>
2024-07-14 02:32:13 +0800

    优化-服务端代码

--- api/db.js
+++ api/db.js
@@ -1,8 +1,15 @@
 import mysql from 'mysql'
 
 export const db = mysql.createConnection({
-    host: "localhost",
+    host: "144.202.124.215",
     user: "root",
-    password: "123456",
-    database: "blog"
+    password: "Wowangle123!",
+    database: "BrianBlog"
 })
+
+// export const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "123456",
+//     database: "blog"
+// })
--- api/index.js
+++ api/index.js
@@ -1,13 +1,41 @@
 import express from "express"
+import cors from 'cors';  // 引入 cors 中间件
 import postsRoutes from './routes/posts.js'
 import usersRoutes from './routes/users.js'
 import authRoutes from './routes/auth.js'
 import cookieParser from 'cookie-parser'
+import multer from 'multer'
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "../client/my-react-app/public/upload");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+app.use(cors({
+    origin: 'http://144.202.124.215',  // 允许访问的源
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',  // 允许的方法
+    allowedHeaders: 'Content-Type,Authorization',  // 允许的请求头
+    credentials: true  // 允许携带凭证（如 Cookies）
+}));  // 允许所有来源的请求
+
+app.options('*', cors());  // 允许所有来源的 OPTIONS 请求
+
+app.post("/api/upload", upload.single("file"), function (req, res) {
+    const file = req.file;
+    res.status(200).json(file.filename);
+  });
+
 app.use('/api/posts', postsRoutes)
 app.use('/api/users', usersRoutes)
 app.use('/api/auth', authRoutes)
